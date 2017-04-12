c59db1d0b29c22d79e5d36ce41f12e847de4410d
Yadong1132 <278742435@qq.com>
2017-04-12 14:24:13 +0800

    Revert "Revert "yy""
    This reverts commit a224512a6c165cdcd47752cc558b882328f56a8b.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+define('VIEW','App/views/');
+define('CONTROLLER','App/controller/');
+define('MODEL','App/model/');
+define('CSS_PATH','/Static/default/css/');
+define('JS_PATH','/Static/default/js/');
+define('IMG_PATH','/Static/default/images/');
+define('FONT_PATH','/Static/default/fonts/');
+
+
+require 'Cord/common.php';
+//require '/DBPdo.php';
+require 'Cord/Framework.php';
+
+Cord\Framework::start();
+
+
+
+
+
+
